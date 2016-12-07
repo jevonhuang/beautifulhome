2bb25052d37072ac1b9f434a248a336a2d6780a1
hfeng <97651906@qq.com>
2016-12-07 12:18:34 +0800

    lua string.split

---
+++ code/utility/lua/string.lua
@@ -0,0 +1,16 @@
+function string.split(str, delimiter)
+    str = tostring(str)
+    delimiter = tostring(delimiter)
+    if delimiter == '' then 
+		return {} 
+	end
+	
+    local pos = 0
+	local arr = {}
+    for st, sp in function() return string.find(str, delimiter, pos, true) end do
+        table.insert(arr, string.sub(str, pos, st - 1))
+        pos = sp + 1
+    end
+    table.insert(arr, string.sub(str, pos))
+    return arr
+end
