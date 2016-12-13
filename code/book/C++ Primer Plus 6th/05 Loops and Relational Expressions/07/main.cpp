386d519ab92b2b04a7bd79cad051c5a7a4f0f2ad
hfeng <97651906@qq.com>
2016-12-13 14:32:53 +0800

    c++ primer plus chapter 05 exercise 07

---
+++ code/book/C++ Primer Plus 6th/05 Loops and Relational Expressions/07/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Car
+{
+	string make;
+	int year;
+};
+
+int main()
+{
+	cout << "How many cars do you wish to catalog? ";
+	int nums = 0;
+	cin >> nums;
+	cin.get();
+	
+	Car* cars = new Car[nums];
+	
+	for (int i = 0; i < nums; ++i)
+	{
+		cout << "Car #" << (i + 1) << ":" << endl;
+		cout << "Please enter the make: ";
+		getline(cin, cars[i].make);
+		cout << "Please enter the year made: ";
+		cin >> cars[i].year;
+		cin.get();
+	}
+	
+	cout << "Here is your collection:" << endl;
+	for (int i = 0; i < nums; ++i)
+	{
+		cout << cars[i].year << " " << cars[i].make << endl;
+	}
+}
