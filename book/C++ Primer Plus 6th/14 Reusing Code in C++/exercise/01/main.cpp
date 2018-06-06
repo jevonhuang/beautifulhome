885072ca0ed8b9bbf977ace82ca1c588db25dcfa
hfeng <97651906@qq.com>
2018-06-06 14:59:42 +0800

    book/C++ Primer Plus 6th/14 Reusing Code in C++/exercise/01

--- book/C++ Primer Plus 6th/14 Reusing Code in C++/exercise/01/main.cpp
+++ book/C++ Primer Plus 6th/14 Reusing Code in C++/exercise/01/main.cpp
@@ -1,7 +1,32 @@
+#include <iostream>
 #include "wine.h"
+
+using namespace std;
 
 int main()
 {
+	cout << "Enter name of wine: ";
+	char lab[64];
+	cin.getline(lab, 64);
+	cout << "Enter number of years: ";
+	int years;
+	cin >> years;
+
+	Wine holding(lab, years);
+	holding.GetBottles();
+	holding.Show();
+	
+	const int YRS = 3;
+	int y[YRS] = {1993, 1995, 1998};
+	int b[YRS] = {48, 60, 72};
+	
+	Wine more("Gushing Grape Red", YRS, y, b);
+	more.Show();
+	
+	cout << "Total bottles for " << more.Label() << ": " << more.Sum() << endl;
+	
+	cout << "Bye\n";
+	
 	return 0;
 }
 
--- book/C++ Primer Plus 6th/14 Reusing Code in C++/exercise/01/wine.h
+++ book/C++ Primer Plus 6th/14 Reusing Code in C++/exercise/01/wine.h
@@ -1,6 +1,7 @@
 #ifndef WINE_H_
 #define WINE_H_
 
+#include <iostream>
 #include <string>
 #include <valarray>
 #include "pair.h"
@@ -12,13 +13,52 @@ typedef Pair<ArrayInt, ArrayInt> PairArray;
 
 public:
 	Wine() {}
-	Wine(const char* l, int y, const int yr[], const int bot[]);
-	Wine(const char* l, int y);
+	Wine(const char* l, int y, const int yr[], const int bot[]) : lable(l), years(y), bottles(ArrayInt(y), ArrayInt(y))
+	{
+		for (int i = 0; i < y; i++)
+		{
+			bottles.first()[i] = yr[i];
+			bottles.second()[i] = bot[i];
+		}
+	}
+	
+	
+	Wine(const char* l, int y) : lable(l), years(y), bottles(ArrayInt(y), ArrayInt(y))
+	{
+	}
 	
 public:
-	void GetBottles();
-	const std::string& Label();
-	int sum();
+	void GetBottles()
+	{
+		std::cout << "Enter " << lable << " date for " << years << " years:" << std::endl;
+		for (int i = 0; i < years; i++)
+		{
+			std::cout << "Enter year: ";
+			std::cin >> bottles.first()[i];
+			
+			std::cout << "Enter bottles for that year: ";
+			std::cin >> bottles.second()[i];
+		}
+	}	
+	
+	const std::string& Label()
+	{
+		return lable;
+	}	
+	
+	int Sum()
+	{
+		return bottles.second().sum();
+	}
+	
+	void Show()
+	{
+		std::cout << "Wine: " << lable << std::endl;
+		for (int i = 0; i < years; i++)
+		{
+			std::cout << "\t" << bottles.first()[i] << "\t" << bottles.second()[i] << std::endl;
+		}
+	}	
 private:
 	std::string lable;
 
