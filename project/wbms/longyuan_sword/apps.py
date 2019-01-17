001d229ca816339f007e787b8ad05638651e04f0
huangxianfeng <280828372@qq.com>
2019-01-17 19:00:56 +0800

    后台系统

---
+++ project/wbms/longyuan_sword/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LongyuanSwordConfig(AppConfig):
+    name = 'longyuan_sword'
---
+++ project/wbms/longyuan_sword/views.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return render(request, 'index.html')
+	
+def add(request):
+	a = request.GET['a']
+	b = request.GET['b']
+	c = int(a) + int(b)
+	
+	return HttpResponse(str(c))
