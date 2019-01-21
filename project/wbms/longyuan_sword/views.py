afd41be674a8910afb067eef6411876a59433ad5
huangxianfeng <280828372@qq.com>
2019-01-21 15:40:18 +0800

    登陆加密

--- project/wbms/longyuan_sword/views.py
+++ project/wbms/longyuan_sword/views.py
@@ -1,6 +1,7 @@
 # coding:utf-8
+import hashlib
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 
 def index(request):
 	return render(request, 'index.html')
@@ -8,6 +9,19 @@ def index(request):
 def login(request):
 	user = request.GET['user']
 	password = request.GET['password']
+	
+	#账号验证
+	if user == 'admin':
+		if password != hashlib.md5(b'176(love)017').hexdigest():
+			return HttpResponseRedirect("/")
+		#账号界面
+		
+	else:
+		#数据库验证
+		
+		#系统界面
+		pass
+	
 	return HttpResponse(password)
 	
 def add(request):
